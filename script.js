42da58c3d4abaa71d6c50756004bd886e68bd1cc
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-09-17 01:20:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function getFormvalue() {
-    //Write your code here
-	const name = document.getElementByName('fname').value;
-	const lastNme = document.getElementByName('lname').value;
-	alert(`${name} ${lastNme}`);
+function getFormvalue(event) {
+	 event.preventDefault(); // Prevents the page from reloading
+
+      const firstName = document.getElementsByName('fname')[0].value;
+      const lastName = document.getElementsByName('lname')[0].value;
+
+      alert(`${firstName} ${lastName}`);
 
 }
