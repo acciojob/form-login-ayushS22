bce3d8a7fa1c611afca808dde3ec55ad0a17e0a2
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-09-17 01:17:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	const name = document.getElementByName('fname').value;
+	const lastNme = document.getElementByName('lname').value;
+	alert(`${name} ${lastNme}`);
 
 }
